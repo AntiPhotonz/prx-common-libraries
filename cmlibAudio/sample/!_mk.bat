63d6b9d345a9b11676fdf50475d760e238ede08d
ABCanG <abcang1015@gmail.com>
2011-07-02 21:26:37 +0900

    rm cmlibAudio

--- cmlibAudio/sample/!_mk.bat
+++
@@ -1,8 +0,0 @@
-PATH=C:\pspsdk\bin\;C:\pspsdk\psp\bin\
-make clean
-make > log.txt 2>&1
-::C:\pspsdk\psp\sdk\psp-packer\bin\psp-packer *.prx
-del *.o
-del *.elf
-start log.txt
-exit
--- cmlibAudio/src/!_mk.bat
+++
@@ -1,8 +0,0 @@
-PATH=C:\pspsdk\bin\;C:\pspsdk\psp\bin\
-make clean
-make > log.txt 2>&1
-psp-build-exports -s exports.exp
-del *.o
-del *.elf
-start log.txt
-exit
