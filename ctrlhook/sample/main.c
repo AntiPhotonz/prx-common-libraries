58c7c1335c3729fe44af3fc5f4cc599d71643071
plum429 <plum429@localhost>
2011-07-09 21:14:49 +0900

    add ctrlhook

---
+++ ctrlhook/sample/main.c
@@ -0,0 +1,71 @@
+// ctrlhook_sample		by plum
+
+// header
+#include <pspkernel.h>
+#include <pspctrl.h>
+#include <pspdisplay_kernel.h>
+#include "ctrlhook.h"
+
+// info
+PSP_MODULE_INFO("test_ctrl", 0x1000, 0, 1);
+
+// global
+CTRL_HANDLER previous;
+
+// function
+int test_func(SceCtrlData *pad)
+{
+	if(pad->Buttons & PSP_CTRL_RTRIGGER)
+	{
+		pad->Buttons |= PSP_CTRL_RIGHT;
+	}
+
+	else if(pad->Buttons & PSP_CTRL_LTRIGGER)
+	{
+		pad->Buttons |= PSP_CTRL_LEFT;
+	}
+
+	return (previous) ? previous(pad) : 0;
+}
+
+int main_thread(SceSize args, void *argp)
+{
+	SceCtrlData pad;
+	int level = 30;
+
+	while(1)
+	{
+		sceKernelDelayThread(50000);
+		GetRawCtrlData(&pad);
+
+		if(pad.Buttons & PSP_CTRL_CIRCLE)
+		{
+			level++;
+			if(level > 99) level = 30;
+
+			sceDisplaySetBrightness(level, 0);
+		}
+	}
+
+	return 0;
+}
+
+int module_start(SceSize args, void *argp)
+{
+	// patch
+	int res = CtrlControlStartInit();
+
+	if(res == -1)
+		return -1;
+
+	// set
+	previous = CtrlControlSetHandler(test_func);
+	SetDeleteButtons(PSP_CTRL_CIRCLE);
+
+	// thread
+	SceUID thid = sceKernelCreateThread("ctrl_1", main_thread, 32, 0x800, 0, NULL);
+	if(thid >= 0) sceKernelStartThread(thid, args, argp);
+
+	return 0;
+}
+
