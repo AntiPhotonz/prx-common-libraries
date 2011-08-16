dc28bca0a25554d197ea9cc676ebbcf5406b58b5
wn <wnbyaku@gmail.com>
2011-08-16 22:08:54 +0900

    [cmlibMenu] Changed, load font into user memory.

--- cmlibMenu/src/Extension/libmLoadFont.c
+++ cmlibMenu/src/Extension/libmLoadFont.c
@@ -4,8 +4,6 @@
 extern const char *font_cg, *font_hankaku_kana, *font_sjis;
 extern const char *font_icon;
 
-//static int loaded_cg = 0, loaded_hankaku_kana = 0, loaded_sjis = 0, loaded_icon = 0;
-
 int libmLoadFont(int flag){
     SceUID fd;
     SceSize size, readsize;
@@ -13,7 +11,6 @@ int libmLoadFont(int flag){
     char path[64] = "ms0:/seplugins/lib/font/";
     char *font_bin[] = {"cg.bin", "hankaku_kana.bin", "sjis.bin", "icon.bin"};
     int k1;
-    //int *loaded[4] = {&loaded_cg, &loaded_hankaku_kana, &loaded_sjis, &loaded_icon};
     u32 *font_addr[4] = {(u32*)&font_cg, (u32*)&font_hankaku_kana, (u32*)&font_sjis, (u32*)&font_icon};
     
     if(*font_addr[flag - 1]){
@@ -46,7 +43,7 @@ int libmLoadFont(int flag){
     }
     
     // malloc
-    font_buf = psp_malloc(1, size);
+    font_buf = psp_malloc(2, size);
     if(font_buf == NULL){
         sceIoClose(fd);
         pspSdkSetK1(k1);
@@ -64,30 +61,7 @@ int libmLoadFont(int flag){
     }
     
     *font_addr[flag - 1] = (u32)font_buf;
-    //*loaded[flag - 1] = 1;
-    /*
-    switch(flag){
-        case LIBM_FONT_CG:
-            font_cg = font_buf;
-            loaded_cg = 1;
-            break;
-        
-        case LIBM_FONT_HANKAKU_KANA:
-            font_hankaku_kana = font_buf;
-            loaded_hankaku_kana = 1;
-            break;
-        
-        case LIBM_FONT_SJIS:
-            font_sjis = font_buf;
-            loaded_sjis = 1;
-            break;
-        
-        case LIBM_FONT_ICON:
-            font_icon = font_buf;
-            loaded_icon = 1;
-            break;
-    }
-    */
+    
     pspSdkSetK1(k1);
     
     return 0;
